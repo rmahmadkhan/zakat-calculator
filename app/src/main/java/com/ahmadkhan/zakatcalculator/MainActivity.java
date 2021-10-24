5645484e20816ac96dde8ccfb7d919d367b2b7f7
Ahmad Khan <rmahmadkhan@icloud.com>
2021-10-24 15:42:29 +0500

    Defined EditText, TextView and Button variables

--- app/src/main/java/com/ahmadkhan/zakatcalculator/MainActivity.java
+++ app/src/main/java/com/ahmadkhan/zakatcalculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.ahmadkhan.zakatcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextCashInHand = findViewById(R.id.editTextCashInHand);
+        EditText editTextSavings = findViewById(R.id.editTextSavings);
+        EditText editTextGoldSilver = findViewById(R.id.editTextGoldSilver);
+        EditText editTextLoans = findViewById(R.id.editTextLoans);
+
+        Button buttonCalculate = findViewById(R.id.buttonCalculate);
+        TextView textViewPayableZakat = findViewById(R.id.textViewPayableZakat);
     }
 }
